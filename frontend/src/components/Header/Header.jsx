fe31cdbe58f586c6427919ab9697e09db72b321b
heyvipul <vipulgirhestar@gmail.com>
2024-03-02 22:29:05 +0530

    folder

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div>This is Header</div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header/Header.jsx";
+import Topics from "./Topics/Topics.jsx";
+import Info from "./Info/Info.jsx"
+import Blog from "./Blog/Blog.jsx"
+import Testimonials from "./Testimonials/Testimonials.jsx"
+import Footer from "./Footer/Footer.jsx";
+
+export { Header,Topics,Info,Blog,Testimonials,Footer }
