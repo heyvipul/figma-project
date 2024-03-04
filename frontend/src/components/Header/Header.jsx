7fa60fef26155f4d0fc99e7046df447a8d48d0d3
heyvipul <vipulgirhestar@gmail.com>
2024-03-04 22:59:29 +0530

    nav

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../Navbar/Navbar"
 import "./Header.css"
 
 const Header = () => {
   return (
     <header>
       <div className="wrapper">
+        <Navbar/>
         <div className="cta">
           <p className="course-name">Sound Design Masterclass</p>
           <h1>Learn the art of Sound Design</h1>
